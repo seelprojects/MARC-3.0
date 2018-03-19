e72127a2383b87607e8bbf393921d4ac61f9a5bd
Nishant Jha <njha1@lsu.edu>
2018-03-19 15:06:15 -0500

    Missing Files added

---
+++ MARC 3.0/Converter/ClassifyPageConverters.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using MARC2.Model;
+using System.Windows.Media;
+
+namespace MARC2.Converter
+{
+    
+
+    /// <summary>
+    /// Scroll View Height Fix for FR classification page and summarize page
+    /// </summary>
+    public class ScrollViewHalfHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var input = value as System.Double?;
+            if (input == 0)
+            {
+                return input;
+            }
+            return (null != input ? (input - 140)/2.0 : 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //Debugger.Break();
+            return value;
+        }
+    }
+
+    /// <summary>
+    /// Scroll View Height Fix for NFR classification page
+    /// </summary>
+    public class ScrollViewFourthHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var input = value as System.Double?;
+            if (input == 0)
+            {
+                return input;
+            }
+            return (null != input ? (input - 240) / 4.0 : 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //Debugger.Break();
+            return value;
+        }
+    }
+
+}
---
+++ MARC 3.0/Converter/ImportPageConverters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using MARC2.Model;
+using System.Windows.Media;
+
+namespace MARC2.Converter
+{
+
+    /// <summary>
+    /// ScrollView Height Fix
+    /// </summary>
+    public class ScrollViewHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var input = value as System.Double?;
+            if (input == 0)
+            {
+                return input;
+            }
+            return (null != input ? input - 70 :0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //Debugger.Break();
+            return value;
+        }
+    }
+}
